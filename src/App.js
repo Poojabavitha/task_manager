cdcfbc1547af764bd3e87e6a0ab7a33e00343e98
Poojabavitha <bavithashanmugam19@gmail.com>
2024-10-14 20:30:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import {firebase} from './firebase.js';
+// import {getAuth,
+//   signInWithEmailAndPassword,
+//   signOut,
+//   onAuthStateChanged } from 'firebase/auth';
+  // import {BrowserRouter, Routes, Route} from 'react-router-dom';
+  import SignIn from './component/SignIn.js';
+  import SignUp from './component/SignUp.js';
+  import { useState } from 'react';
 
 function App() {
+
+  const [component, switchComponent] = useState(()=>'signin');
+
+  function toogleAuthComp(){
+    switchComponent(prev=>prev==='signin'? 'signup': 'signin')
+  }
+
+  function signIn(){
+
+  }
+  function signUp(){
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div>
+    <button onClick={toogleAuthComp}>SignIn</button>
+    <button onClick={toogleAuthComp}>SignUp</button>
+  </div>
+    {
+      component=='signin'?(<SignIn signIn={signIn}/>) : (<SignUp signUp={signUp}/>)
+    }
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCs2LMzJ98U2qrp_9-DACpsvcOTXsiUt6Q",
+  authDomain: "task-manager-482d8.firebaseapp.com",
+  projectId: "task-manager-482d8",
+  storageBucket: "task-manager-482d8.appspot.com",
+  messagingSenderId: "611654576630",
+  appId: "1:611654576630:web:ccd49825b8e7eef5ab3fd7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
